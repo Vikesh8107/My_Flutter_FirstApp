d96c2180abc399595dde76bb7b45baeaf4538272
Vikesh8107 <anonymsdc123@gmail.com>
2023-12-05 20:57:34 +0530

    Version 1.1..0

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,128 @@
-// lib/main.dart
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:math'; // Import the dart:math library
+
 import 'package:flutter/material.dart';
-import 'screens/input_screen.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Stock Viewer',
+      title: 'Stocks App',
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.blue,
       ),
-      home: InputScreen(),
+      home: StockPage(),
+    );
+  }
+}
+
+class StockPage extends StatefulWidget {
+  @override
+  _StockPageState createState() => _StockPageState();
+}
+
+class _StockPageState extends State<StockPage> {
+  static const String apiKey = 'Uuc2J73WWc22LcUhvpmhZcPjPptbRLbE';
+  static const String apiUrl =
+      'https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-01-09?adjusted=true&apiKey=$apiKey';
+
+  List<Stock> stocks = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+    updateStockPricesPeriodically();
+  }
+
+  Future<void> fetchData() async {
+    try {
+      final response = await http.get(Uri.parse(apiUrl));
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        // Assuming the data structure is as mentioned in your example.
+        final List<dynamic> results = data['results'];
+        stocks = results.map((result) => Stock.fromJson(result)).toList();
+        updateStockPricesInFile();
+      } else {
+        print('Failed to fetch stocks: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error fetching stocks: $error');
+    }
+  }
+
+  Future<void> updateStockPricesInFile() async {
+    final file = File('stock_prices.json');
+    final List<Map<String, dynamic>> stockDataList = [];
+
+    for (final stock in stocks) {
+      stock.refreshInterval = Duration(seconds: Random().nextInt(5) + 1);
+      stock.currentPrice = stock.previousClose + Random().nextDouble();
+
+      stockDataList.add({
+        'name': stock.name,
+        'price': stock.currentPrice,
+      });
+    }
+
+    await file.writeAsString(json.encode(stockDataList));
+    // Call setState to trigger a UI update.
+    setState(() {});
+  }
+
+  void updateStockPricesPeriodically() {
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      updateStockPricesInFile();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stocks'),
+      ),
+      body: ListView.builder(
+        itemCount: stocks.length,
+        itemBuilder: (context, index) {
+          final stock = stocks[index];
+          return ListTile(
+            title: Text('${stock.name} - \$${stock.currentPrice.toStringAsFixed(2)}'),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class Stock {
+  String name;
+  double previousClose;
+  Duration refreshInterval;
+  double currentPrice;
+
+  Stock({
+    required this.name,
+    required this.previousClose,
+    required this.refreshInterval,
+    required this.currentPrice,
+  });
+
+  factory Stock.fromJson(Map<String, dynamic> json) {
+    return Stock(
+      name: json['T'],
+      previousClose: json['c'].toDouble(),
+      refreshInterval: Duration(seconds: 1), // Default to 1 second.
+      currentPrice: 0.0, // Default to 0.0.
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,21 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:my/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('StockPage displays stock information', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MaterialApp(home: StockPage()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that the StockPage is displayed.
+    expect(find.byType(StockPage), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // For simplicity, let's assume the first stock is displayed in the list.
+    // Replace with actual test logic based on your UI structure.
+    expect(find.text('Stock0'), findsOneWidget);
+    expect(find.textContaining('Price: \$'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // You can add more test cases based on your UI structure and requirements.
   });
 }
